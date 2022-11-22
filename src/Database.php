d04958aa6946c7b243971a74e1a444b65d9a6009
dineshuprety <dineshuprety@users.noreply.github.com>
2022-11-22 12:28:48 +0000

    Fix styling

--- src/Database.php
+++ src/Database.php
@@ -5,13 +5,16 @@ namespace Nepo\Helper;
 class Database
 {
     protected string $host;
+
     protected string $database;
+
     protected string $username;
+
     protected string $password;
+
     protected int $port;
 
-    public function __construct($host,$database, $username, $password, $port = null)
+    public function __construct($host, $database, $username, $password, $port = null)
     {
-
     }
 }

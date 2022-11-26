10115252fda6f8a6ffeffd62d3d927ba92bdf63a
dineshuprety <dineshuprety500@gmail.com>
2022-11-26 16:48:25 +0545

    working on database con

--- src/Database.php
+++ src/Database.php
@@ -28,7 +28,7 @@ class Database
     public function __construct($host, $database, $username, $password, $port = null)
     {
         try {
-            $this->con = new PDO("mysql:host=" . $this->host . "; port=".($this->$port)? '3306' : $port ."; dbname=" . $this->database, $this->username, $this->password);
+            $this->con = new PDO("mysql:host=" . $this->host . "; port=" . (string)($this->$port) ? '3306' : $port . "; dbname=" . $this->database, $this->username, $this->password);
         } catch (\Exception $e) {
             echo "Database Connection Problems" . $e->getMessage();
         }

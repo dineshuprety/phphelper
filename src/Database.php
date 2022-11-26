07bbc4808beaf99d8effdca63cf53e088e3beaea
dineshuprety <dineshuprety500@gmail.com>
2022-11-26 16:43:48 +0545

    working on database con

--- src/Database.php
+++ src/Database.php
@@ -4,17 +4,33 @@ namespace Nepo\Helper;
 
 class Database
 {
-    protected string $host;
+    private string $host;
 
-    protected string $database;
+    private string $database;
 
-    protected string $username;
+    private string $username;
 
-    protected string $password;
+    private string $password;
 
-    protected int $port;
+    private int $port;
 
+    protected $con;
+
+    /**
+     * It's a constructor for a class that connects to a database.
+     * 
+     * @param host The hostname of the database server.
+     * @param database The name of the database you want to connect to.
+     * @param username The username for the database
+     * @param password The password for the database user.
+     * @param port The port number to use when connecting to the database.
+     */
     public function __construct($host, $database, $username, $password, $port = null)
     {
+        try {
+            $this->con = new PDO("mysql:host=" . $this->host . "; port=".($this->$port)? '3306' : $port ."; dbname=" . $this->database, $this->username, $this->password);
+        } catch (\Exception $e) {
+            echo "Database Connection Problems" . $e->getMessage();
+        }
     }
 }
